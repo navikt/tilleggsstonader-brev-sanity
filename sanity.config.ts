51d2bde2e86ec6fb94c15e3b099554de747b0c72
eirarset <eirik.arseth@nav.no>
2023-09-26 12:25:10 +0200

    Skal kunne logge på sanity studio med NAV sso

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'sanity'
+import { AuthConfig, defineConfig } from 'sanity'
 import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/schemas'
+
+const auth: AuthConfig = {
+  redirectOnSingle: true,
+  providers: () => [
+    {
+      name: 'saml',
+      title: 'NAV SSO',
+      url: 'https://api.sanity.io/v2021-10-01/auth/saml/login/f3270b37',
+      logo: '/static/navlogo.svg',
+    },
+  ],
+  loginMethod: 'dual',
+};
 
 export default defineConfig({
   name: 'default',
@@ -15,4 +28,5 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
+  auth: auth
 })
