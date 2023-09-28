55b07a5de2a552e4077dc0f47489d5a21a8421be
sarahjelle <sara.hjelle@bekk.no>
2023-09-28 07:45:55 +0200

    Legge til visningsdetaljer i delmal

--- src/schemas/delmal.ts
+++ src/schemas/delmal.ts
@@ -1,11 +1,32 @@
+import { defineField, defineType } from 'sanity';
+
 import blockEditor from './blockEditor';
 import { DokumentNavn, FeltNavn, SanityTyper } from '../typer';
 
-const Delmal = {
+const Delmal = defineType({
   title: 'Delmal',
   name: DokumentNavn.DELMAL,
-  type: SanityTyper.DOCUMENT,
+  type: 'document',
   fields: [
+    defineField({
+      title: 'Visningsdetaljer',
+      name: 'visningsdetaljer',
+      type: 'object',
+      fields: [
+        defineField({
+          title: 'Skal delmalen alltid være med?',
+          name: 'skalAlltidMed',
+          type: 'boolean',
+          initialValue: false,
+        }),
+        defineField({
+          title: 'Skal delmalen vises i brevmenyen?',
+          name: 'skalVisesIBrevmeny',
+          type: 'boolean',
+          initialValue: false,
+        }),
+      ],
+    }),
     {
       title: 'API-navn',
       name: FeltNavn.API_NAVN,
@@ -13,6 +34,6 @@ const Delmal = {
     },
     blockEditor('nb', 'Bokmål'),
   ],
-};
+});
 
 export default Delmal;
