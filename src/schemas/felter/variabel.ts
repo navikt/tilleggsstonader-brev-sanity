bfa8071c8b4e040ad796eb861acdfd0ad0389141
sarahjelle <sara.hjelle@bekk.no>
2023-09-28 07:55:51 +0200

    Definer felter for variabel

--- src/schemas/felter/variabel.ts
+++ src/schemas/felter/variabel.ts
@@ -1,16 +1,29 @@
-import { DokumentNavn, FeltNavn, SanityTyper } from '../../typer';
+import { defineField, defineType } from 'sanity';
 
-const Variabel = {
+import { DokumentNavn } from '../../typer';
+
+const Variabel = defineType({
   title: 'Variabel',
   name: DokumentNavn.VARIABEL,
-  type: SanityTyper.DOCUMENT,
+  type: 'document',
   fields: [
-    {
+    defineField({
+      title: 'Visningsnavn',
+      name: 'visningsnavn',
+      type: 'string',
+    }),
+    defineField({
       title: 'API-navn',
-      name: FeltNavn.API_NAVN,
-      type: SanityTyper.STRING,
-    },
+      name: 'apiNavn',
+      type: 'string',
+    }),
+    defineField({
+      title: 'Skal variabelen erstattes med HTML-kode?',
+      name: 'erHtml',
+      type: 'boolean',
+      initialValue: false,
+    }),
   ],
-};
+});
 
 export default Variabel;
