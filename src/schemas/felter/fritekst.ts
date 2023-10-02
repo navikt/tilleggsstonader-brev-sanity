fc6742ecb71f48c1fe152b3e94401d0212b6b48e
sarahjelle <sara.hjelle@bekk.no>
2023-10-02 10:53:07 +0200

    Preview av fritekstområde

--- src/schemas/felter/fritekst.ts
+++ src/schemas/felter/fritekst.ts
@@ -1,9 +1,11 @@
+import FritekstPreview from '../../komponenter/FritekstområdePreview';
 import { DokumentNavn } from '../../typer';
 
 const Fritekst = {
   title: 'Fritekst',
   name: DokumentNavn.FRITEKST,
   type: 'document',
+  components: { preview: FritekstPreview },
   fields: [
     { name: 'dummyfelt', type: 'string', initialValue: 'dummyverdi', readOnly: true, hidden: true }, // Brukes ikke til noen ting. Lagt til fordi sanity krever minst et felt
   ],
