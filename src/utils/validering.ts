e325e226d012117a933df19732a55a815a4b7ec3
sarahjelle <sara.hjelle@bekk.no>
2023-10-02 09:21:02 +0200

    Fjerner overflødig validering

--- src/utils/validering.ts
+++ src/utils/validering.ts
@@ -1,23 +1,13 @@
 import groq from 'groq';
 import { StringRule, ValidationContext } from 'sanity';
 
-import { API_NAME_MAX_LENGTH } from './konstanter';
-
 const førsteTegnErLitenBokstavRegex = /^[a-zæøå].*/;
-const kunBokstaverOgTallUtenÆØÅRegex = /^[a-z0-9]+$/i;
 
 const maskinnavnValideringer = (rule: StringRule) => {
   return [
-    rule.required().error('Feltet må settes'),
-    rule
-      .regex(kunBokstaverOgTallUtenÆØÅRegex)
-      .error('Feltet kan kun bestå av tall eller boksaver (ikke æ, ø, å).'),
     rule
       .regex(førsteTegnErLitenBokstavRegex)
       .error('Første tegn i feltet kan ikke være tall eller stor bokstav.'),
-    rule
-      .max(API_NAME_MAX_LENGTH)
-      .error(`Feltet kan være på maksimalt ${API_NAME_MAX_LENGTH} tegn.`),
   ];
 };
 
