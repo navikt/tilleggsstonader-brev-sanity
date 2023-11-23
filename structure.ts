83e07eb1e9f15216246b6cc9c833afe85cde4bfa
sarahjelle <sara.hjelle@bekk.no>
2023-11-23 10:26:06 +0100

    Refaktor. Oppdater navn så de matcher

--- structure.ts
+++ structure.ts
@@ -11,14 +11,14 @@ import { Resultat, Ytelse, ResultatTittel, ytelseTittel } from './src/typer';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const structure = (S: StructureBuilder, _context: StructureResolverContext) => {
   const lagYtelsemappe = lagYtelseMappeFunksjon(S);
-  const lagStegmappe = lagTypemappeFunksjon(S);
+  const lagTypemappe = lagTypemappeFunksjon(S);
 
   return S.list()
     .title('Tilleggsstønader brev')
     .items([
       lagYtelsemappe(Ytelse.BARNETILSYN, [
-        lagStegmappe(Ytelse.BARNETILSYN, Resultat.INNVILGET),
-        lagStegmappe(Ytelse.BARNETILSYN, Resultat.AVSLAG),
+        lagTypemappe(Ytelse.BARNETILSYN, Resultat.INNVILGET),
+        lagTypemappe(Ytelse.BARNETILSYN, Resultat.AVSLAG),
       ]),
       S.divider(),
       ...S.documentTypeListItems().filter((listItem) =>
