ce5898267bf1156a98dbb3f7378b95c965c332cd
Runar Skagestad <runar.skagestad@nav.no>
2026-03-17 08:44:40 +0100

    ⬆️ Bruk node 20->24 og sanity@latest i stedet for @sanity/cli

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,8 +1,8 @@
-import { defineCliConfig } from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 's0grjk9v',
     dataset: 'production',
   },
-})
+});
